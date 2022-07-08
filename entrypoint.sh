dd074b2a2efef34f7c19d4004ec54688320b1d8a
Peter Brightwell <peter.brightwell@bbc.co.uk>
2022-07-08 14:25:25 +0100

    Lint branch in GITHUB_BRANCH env

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,8 @@
-#!/bin/sh -l
+#!/bin/bash -l
 
 export PAGES_REPO_NWO="$GITHUB_REPOSITORY"
-git clone --single-branch "https://github.com/$GITHUB_REPOSITORY" /github-repo
+git clone --single-branch ${GITHUB_BRANCH:+--branch $GITHUB_BRANCH} "https://github.com/$GITHUB_REPOSITORY" /github-repo
 cd /github-repo/.lint || exit
-chmod 777 .
 ln -s /.scripts .scripts
 ln -s /node_modules node_modules
 make lint
