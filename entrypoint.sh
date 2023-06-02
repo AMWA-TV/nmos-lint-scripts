83f6918e8027d7221f532b00bd59d5361fe25b80
Peter Brightwell <peter.brightwell@bbc.co.uk>
2023-06-02 12:00:59 +0100

    Use checkout v3

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,12 @@
 export PAGES_REPO_NWO="$GITHUB_REPOSITORY"
 echo "GITHUB_REF is $GITHUB_REF"
 mkdir /github-repo
-cd /github-repo
+cd /github-repo || exit
 git init --initial-branch=main
 git remote add origin "https://github.com/$GITHUB_REPOSITORY"
 git fetch --depth=1 origin "$GITHUB_REF"
 git checkout FETCH_HEAD
-cd /github-repo/.lint || exit
+cd .lint || exit
 ln -s /.scripts .scripts
 ln -s /node_modules node_modules
 make lint
